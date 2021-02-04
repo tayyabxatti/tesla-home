e548822e051f1a29f25636c8abc5bbc78f221b48
unknown <tayyabmushtaq2@gmail.com>
2021-02-04 18:34:22 +0500

    'header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-
+import Headers from './components/header';
 function App() {
   return (
     <div className="App">
-      <h1>Tayyab</h1>
+      <Headers/>
+      {/* items=>snapping */}
+      {/* items=>snapping */}
+      {/* items=>snapping */}
+      {/* items=>snapping */}
     </div>
   );
 }
 
 export default App;
+  
